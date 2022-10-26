af8cd908996d5ef2bc83caeabfe7a546a61b98d8
Heejeong01110 <123rkdrkd@naver.com>
2022-10-26 19:55:43 +0900

    Initial commit

---
+++ src/test/java/org/movie/reviewer/ReviewerApplicationTests.java
@@ -0,0 +1,13 @@
+package org.movie.reviewer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReviewerApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
